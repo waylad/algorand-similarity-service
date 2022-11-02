65017640724c217322bd7177b1e3176b307d6c59
waylad <waylad42@gmail.com>
2022-11-02 14:35:31 +0100

    Add sdk

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import algosdk from "algosdk";
+import React from "react";
+
+import logo from "./logo.svg";
+
+import "./App.css";
 
 function App() {
+  const algodServer = "https://testnet-algorand.api.purestake.io/ps2";
+  const indexerServer = "https://testnet-algorand.api.purestake.io/idx2";
+  const token = { "X-API-Key": "cnPOsJmkLV99ccOnzgC3d9DOrHyXrs5ka9JB2Vcl" };
+  const port = "";
+
+  const algodClient = new algosdk.Algodv2(token, algodServer, port);
+  const indexerClient = new algosdk.Indexer(token, indexerServer, port);
+
+  const health = algodClient.healthCheck().do();
+  console.log(health);
+
   return (
     <div className="App">
       <header className="App-header">
