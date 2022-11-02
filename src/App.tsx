af8ee70ba7c69ebd74501277c0f0259ecc57cbc9
waylad <waylad42@gmail.com>
2022-11-02 14:40:11 +0100

    Add polyfill

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { TempleDAppNetwork, TempleWallet } from "@temple-wallet/dapp";
 import algosdk from "algosdk";
 import React from "react";
 
@@ -5,36 +6,89 @@ import logo from "./logo.svg";
 
 import "./App.css";
 
-function App() {
-  const algodServer = "https://testnet-algorand.api.purestake.io/ps2";
-  const indexerServer = "https://testnet-algorand.api.purestake.io/idx2";
-  const token = { "X-API-Key": "cnPOsJmkLV99ccOnzgC3d9DOrHyXrs5ka9JB2Vcl" };
-  const port = "";
+type IProps = {};
 
-  const algodClient = new algosdk.Algodv2(token, algodServer, port);
-  const indexerClient = new algosdk.Indexer(token, indexerServer, port);
+type IState = {
+  loading: boolean;
+  wallet?: TempleWallet;
+  tezos?: any;
+  accountPkh?: string;
+};
 
-  const health = algodClient.healthCheck().do();
-  console.log(health);
+export class App extends React.Component<IProps, IState> {
+  public constructor(props: IProps) {
+    super(props);
+    this.state = {
+      loading: false,
+      wallet: undefined,
+      tezos: undefined,
+      accountPkh: undefined,
+    };
+  }
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  connectWallet = async (): Promise<void> => {
+    const algodServer = "https://testnet-algorand.api.purestake.io/ps2";
+    const indexerServer = "https://testnet-algorand.api.purestake.io/idx2";
+    const token = { "X-API-Key": "cnPOsJmkLV99ccOnzgC3d9DOrHyXrs5ka9JB2Vcl" };
+    const port = "";
+
+    const algodClient = new algosdk.Algodv2(token, algodServer, port);
+    const indexerClient = new algosdk.Indexer(token, indexerServer, port);
+
+    const health = await algodClient.healthCheck().do();
+    console.log(health);
+  };
+
+  mintTokens = async (): Promise<void> => {
+    this.setState({
+      ...this.state,
+      loading: true,
+    });
+
+    const counter = await this.state.tezos.wallet.at(
+      "KT1Quos8JNLs94SCTiyN6GdrqeT77yCATS6M"
+    );
+    const operation = await counter.methods
+      .mint(this.state.accountPkh, 1000)
+      .send();
+    await operation.confirmation();
+
+    this.setState({
+      ...this.state,
+      loading: false,
+    });
+  };
+
+  public render(): JSX.Element {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          {this.state.accountPkh ? (
+            <div className="App-connected">
+              <div className="App-address">
+                Connected with {this.state.accountPkh}
+              </div>
+              <button
+                className="App-button"
+                disabled={this.state.loading}
+                onClick={() => this.mintTokens()}
+              >
+                {this.state.loading ? "Loading..." : "Mint some tokens"}
+              </button>
+            </div>
+          ) : (
+            <div className="App-not-connected">
+              <button
+                className="App-button"
+                onClick={() => this.connectWallet()}
+              >
+                Connect Wallet
+              </button>
+            </div>
+          )}
+        </header>
+      </div>
+    );
+  }
 }
-
-export default App;
